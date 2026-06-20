ae751f24e4eb351590e9fc2e79c2a8b845101b6b
Anita Marvar <155180187+AnitaMarvar@users.noreply.github.com>
2026-06-20 15:27:37 +0530

    Create Majority element II.cpp

---
+++ Day 6/Majority element II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size() / 3;
+        vector<int>ans;
+
+        unordered_map<int,int>mp;
+        for(int x : nums)
+        {
+            mp[x]++;
+        }
+        for(auto it : mp)
+        {
+            if(it.second > n)
+            {
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
