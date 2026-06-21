c50d8c693fa86d93ff7145492f08606cbf672fbe
Anita Marvar <155180187+AnitaMarvar@users.noreply.github.com>
2026-06-21 20:44:08 +0530

    Create Median of 2 sorted arrays.cpp

---
+++ Day 21/Median of 2 sorted arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>arr;
+        for(int x : nums1)
+        {
+            arr.push_back(x);
+        }
+        for(int x : nums2)
+        {
+            arr.push_back(x);
+        }
+        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        if( n % 2 == 1)
+        {
+            return (double)arr[n/2];
+        }
+        return (double)((arr[n/2] + arr[(n/2)-1])/2.0000);
+
+        
+    }
+};
