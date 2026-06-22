fddd382212719dc9f1348e055e66796f7cc06ba6
Anita Marvar <155180187+AnitaMarvar@users.noreply.github.com>
2026-06-22 12:56:33 +0530

    Update maximum sum combination.cpp

--- Day 22/maximum sum combination.cpp
+++ Day 22/maximum sum combination.cpp
@@ -1,4 +1,4 @@
-//tle 
+//tle --> brute force
 
 class Solution {
   public:
@@ -30,6 +30,49 @@ class Solution {
         }
         reverse(ans.begin(),ans.end());
         return ans;
-        
     }
+
+    //optimal approach
+    
+class Solution {
+  public:
+  
+    vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+        vector<int>ans;
+        
+        sort(a.begin(),a.end(), greater<int>());
+        sort(b.begin(), b.end(),greater<int>());
+        
+        int n=a.size();
+        //maxheap;
+        priority_queue<tuple<int,int,int>>maxheap;
+        int maxSum = a[0] + b[0];
+
+        set<pair<int,int>>vis;  //to avoid visiting the elements again
+        
+        maxheap.push({maxSum, 0,0});
+        vis.insert({0,0});
+        
+        while(k-- && !maxheap.empty())
+        {
+            auto [maxSum, i,j] = maxheap.top();
+            maxheap.pop();
+            ans.push_back(maxSum);
+            
+            if(i+1 < n && !vis.count({i+1,j}))
+            {
+                maxSum = a[i+1] + b[j];
+                maxheap.push({maxSum, i+1,j});
+                vis.insert({i+1,j});
+            }
+            if(j+1 < n && !vis.count({i,j+1}))
+            {
+                maxSum  = a[i] + b[j+1];
+                maxheap.push({maxSum, i,j+1});
+                vis.insert({i,j+1});
+            }
+        }
+        return ans;
+    }
+};
 };
