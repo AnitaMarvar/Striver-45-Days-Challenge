13a5c8834f4a22f91c49a2e8d25ea87856e65964
Anita Marvar <155180187+AnitaMarvar@users.noreply.github.com>
2026-06-29 20:59:30 +0530

    Create stock span.cpp

---
+++ Day 28/stock span.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+       //previous greater element
+       int n=arr.size();
+       vector<int>ans(n);
+       
+       stack<int>st;
+       
+       for(int i=0;i<n;i++)
+       {
+           while(!st.empty() && arr[st.top()] <= arr[i])
+           {
+               st.pop();
+           }
+           if(st.empty())
+           {
+               ans[i] = i+1;
+           }
+           else{
+               ans[i] = i - st.top();
+           }
+           st.push(i);
+       }
+       return ans;
+       
+    }
+};
